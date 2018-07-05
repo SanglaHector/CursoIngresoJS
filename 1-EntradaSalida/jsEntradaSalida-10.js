ed1891030d8beb054512197ba75abe64cc990168
sanglahector <sanglahector@gmail.com>
2018-07-05 11:20:11 -0300

    ejercicio 10

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,11 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+    var importe;
+    var porcentaje;
+    var resultado;
+    importe=document.getElementById("importe").value;
+    porcentaje=0.25;
+    resultado=importe-(importe*porcentaje);
+	document.getElementById("resultado").value=resultado
 }
