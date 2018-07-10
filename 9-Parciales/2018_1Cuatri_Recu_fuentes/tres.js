5966d1af7b219c37a3c323594ae2b09e213b8c82
sanglahector <sanglahector@gmail.com>
2018-07-10 09:50:37 -0300

    recuperatorios del 1 al 3

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,35 @@
 function mostrar()
 {
+    var precio;
+    
+    var porcentaje;
+    
+    var descuento;
+
+    var precioConDescuento;
+
+    var iva;
+
+    var precioConDescuentoMasIva;
+
+    precio = prompt("Ingrese el precio");
+
+    porcentaje = prompt("Ingrese el porcentaje");
+
+    precio = parseInt(precio);
+
+    porcentaje = parseInt(porcentaje);
+    
+    descuento = precio * (porcentaje / 100);
+
+    precioConDescuento = precio - descuento;
+
+    iva = 21;
+
+    precioConDescuentoMasIva = precioConDescuento +  ( precio * (iva / 100));
+
+    alert( "Su descuento es de " + descuento + ", el precio con descuento es de " + precioConDescuento + " y el IVA es igual a " + iva);
+
+    alert( "El precio final es de " + precioConDescuentoMasIva );
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,31 @@
 
 function mostrar()
 {
+    var base;
+
+    var altura;
+
+    var superficie;
+
+    var perimetro;
+
+    var lado;
+
+    base = prompt("Ingrese la base del triangulo");
+
+    altura = prompt("ingrese la altura del triangulo");
+
+    base = parseInt(base);
+
+    altura = parseInt(altura);
+
+    lado = base
+
+    perimetro = lado * 3
+
+    superficie = ( base * altura ) / 2;
+
+    alert("El perimetro es igual a " + perimetro +" y la superficie es igual a " + superficie)
+
 
 }
