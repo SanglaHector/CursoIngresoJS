d712c9359d3945336e7637c97a08f5ef5268f900
sanglahector <sanglahector@gmail.com>
2018-07-26 11:29:47 -0300

    ejercicio 10 teminado

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -8,35 +8,65 @@ function mostrar()
 	var numero; 
 	var positivo = 0;
 	var negativo = 0;
-	var contadorDeceros = 0;
+	var contadorDeCeros = 0;
 	var contadorDePositivos = 0;
-	var contadorDeNegarivos = 0;
+	var contadorDeNegativos = 0;
 	var diferencia;
 	var promedioPositivo;
 	var promedioNegativo;
-	var numerosPares;
+	var numerosPares = 0;
+	var msg
 	while(respuesta!="no")
 	{
 		numero = prompt("Ingrese un numero");
 		numero = parseInt(numero);
 
 	 
-		while(inNan(numero)){
+		while(isNaN(numero)){
 			numero = prompt("ingrese un numero correcto");
 			numero = parseInt(numero);
 			
 }
 	if(numero < 0 ){
-		contadorDeNegarivos ++
+		contadorDeNegativos ++
 		negativo = numero + negativo;
 
-	}else if(numero > 0){
+	}
+	else if(numero > 0){
 		contadorDePositivos ++ ;
 		positivo = positivo + numero;
-	}else if(numero == 0){
+	}
+	else if(numero == 0){
 		contadorDeCeros ++ ;
 	}
-	
+	if(numero % 2 == 0){
 
+
+		numerosPares ++ ;
 	}
-}}//FIN DE LA FUNCIÓN
+	promedioNegativo = negativo / contadorDeNegativos;
+	promedioPositivo = positivo / contadorDePositivos;
+	 diferencia= contadorDePositivos - contadorDeNegativos;
+	
+	if ( diferencia > 0){
+		msg = "hay mas numeros negativos por " + (diferencia *-1) + " de cantidad";
+	}
+	else if( diferencia < 0){
+		msg = "hay mas numeros positivos por " +(diferencia) + " de cantidad"; 
+	
+	}
+		else {
+			msg = "No hay diferencia entre positivos y negativos";
+		}
+		respuesta = prompt( "¿Desea ingresar otro numero?");
+}
+document.write("Suma de negativos:" + negativo + "<br>");
+	document.write("Suma de positivos:" + positivo + "<br>");
+	document.write("Cantidad de n° negativos:" + contadorDeNegativos + "<br>");
+	document.write("Cantidad de n° positivos:" +contadorDePositivos + "<br>");
+	document.write("Cantidad de Ceros:" + contadorDeCeros + "<br>");
+	document.write("Cantidad de n° pares:" + numerosPares + "<br>");
+	document.write("Promedio de los n° negativos:" + promedioNegativo + "<br>");
+	document.write("Promedio de n° positivos:" + promedioPositivo + "<br>");
+	document.write(msg);
+}//FIN DE LA FUNCIÓN
